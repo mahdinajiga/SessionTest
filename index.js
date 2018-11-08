60cf5aa43e496ca2acc0099ce5c87e5f676dbc28
MahdiNaji <mahdinajiga@yahoo.com>
2018-11-08 21:15:17 +0330

    04

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ var upload = multer();
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var MemcachedStore = require('connect-memcached')(session);
+var bcrypt = require('bcrypt');
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    passwordConf: {
+        type: String,
+        required: true,
+    }
+});
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -17,11 +43,11 @@ app.use(cookieParser());
 app.use(session({
     secret: 'E7V49SX1CSV68DFS79TG46E5T3DSG1G3',
     key: 'user_sid',
-    proxy   : 'true',
-    store   : new MemcachedStore({
-      hosts: ['127.0.0.1:11211'], //this should be where your Memcached server is running
-      secret: '79DSSEE6V37V49G1GG468DX1CFS5T3ST' // Optionally use transparent encryption for memcache session data 
-  })
+    proxy: 'true',
+    store: new MemcachedStore({
+        hosts: ['127.0.0.1:11211'], //this should be where your Memcached server is running
+        secret: '79DSSEE6V37V49G1GG468DX1CFS5T3ST' // Optionally use transparent encryption for memcache session data 
+    })
 }));
 
 
@@ -52,23 +78,46 @@ app.post('/signup', function (req, res) {
         res.render('signup', { message: "User Already Exists! Login or choose another user id" });
     }
     else {
-    if (!req.body.id || !req.body.password) {
-        res.status("400");
-        res.send("Invalid details!");
-    } else {
-        Users.filter(function (user) {
-            if (user.id === req.body.id) {
-                res.render('signup', {
-                    message: "User Already Exists! Login or choose another user id"
-                });
-            }
-        });
-        var newUser = { id: req.body.id, password: req.body.password };
-        Users.push(newUser);
-        req.session.user = newUser;
-        res.redirect('/protected_page');
+        if (!req.body.id || !req.body.password) {
+            res.status("400");
+            res.send("Invalid details!");
+        } else {
+            /*
+            Users.filter(function (user) {
+                if (user.id === req.body.id) {
+                    res.render('signup', {
+                        message: "User Already Exists! Login or choose another user id"
+                    });
+                }
+            });
+            var newUser = { id: req.body.id, password: req.body.password };
+            Users.push(newUser);
+            
+            var userData = {
+                email: "info@mahdinaji.com",
+                username: req.body.id,
+                password: req.body.password,
+                passwordConf: req.body.password,
+            }*/
+            var newUser = new User({email: "info@mahdinaji.com",
+                username: req.body.id,
+                password: req.body.password,
+                passwordConf: req.body.password
+            });
+            
+            newUser.save(function (err,User) {
+                if(err)
+                {
+                    res.render('signup', {message: "Database error"});
+                }
+                else
+                {
+                    req.session.user = userData;
+                    res.redirect('/protected_page');
+                }
+            });
+        }
     }
-}
 });
 
 
@@ -90,12 +139,24 @@ app.post('/login', function (req, res) {
         if (!req.body.id || !req.body.password) {
             res.render('login', { message: "Please enter both id and password" });
         } else {
-            Users.filter(function (user) {
-                if (user.id === req.body.id && user.password === req.body.password) {
-                    req.session.user = user;
-                    res.redirect('/protected_page');
+
+            User.find({username : req.body.id},function (err,res) {
+                if(err)
+                {
+                    console.log(err);
+                } 
+                else
+                {
+                    res.forEach(element => {
+                        if(element.password == req.body.password)
+                        {
+                            req.session.user = user;
+                            res.redirect('/protected_page');
+                        }                        
+                    });
                 }
             });
+
             res.render('login', { message: "Invalid credentials!" });
         }
     }
